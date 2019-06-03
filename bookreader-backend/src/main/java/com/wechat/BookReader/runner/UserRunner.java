f7dc2403dc073196588626eadd6200f4861cfbe1
xyh <467946553@qq.com>
2019-06-03 18:40:30 +0800

    fix:更新了用户姓名

---
+++ bookreader-backend/src/main/java/com/wechat/BookReader/runner/UserRunner.java
@@ -0,0 +1,22 @@
+package com.wechat.BookReader.runner;
+
+import com.wechat.BookReader.dao.UserRepo;
+import com.wechat.BookReader.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(value = 3)
+public class UserRunner implements ApplicationRunner {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        User user = new User("test","test");
+        userRepo.save(user);
+    }
+}
